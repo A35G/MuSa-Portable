74f084a0b0847128f5483dcfe0073c7f796cf744
Gianluigi <a35g@hackworld.it>
2024-04-24 22:32:12 +0200

    Create musa.en-US.js
    English language file

---
+++ assets/js/i18n/musa.en-US.js
@@ -0,0 +1,24 @@
+var messages = {
+    "musa-title": "MuSa Project",
+    "table-title": "Title",
+    "table-year": "Year",
+    "table-duration": "Duration",
+    "table-head-nav": "entries per page",
+    "table-no-data": "No data",
+    "table-footer-page": "Go to page:",
+    "table-footer-show": "Showing ",
+    "table-footer-to": " to ",
+    "table-footer-of": " of ",
+    "table-footer-entries": " entries",
+    "table-footer-prevbutt": "Previous",
+    "table-footer-nextbutt": "Next",
+    "head-genre-filter": "Genre",
+    "head-year-filter": "Year",
+    "head-search": "Search..."
+};
+
+Musa.prototype.loadLocale = function() {
+    if (typeof messages !== '' && messages !== 'undefined') {
+        this.translations = messages;
+    }
+}
